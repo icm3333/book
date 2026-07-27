715deeee9d49afa18c23449d701aa673275dc4ed
Iris Cardoso <trashhyuki@gmail.com>
2026-07-27 20:00:23 -0300

    chore: .env creation + gitignore update

--- src/main/java/rafael/book/BookApplication.java
+++ src/main/java/rafael/book/BookApplication.java
@@ -1,5 +1,6 @@
 package rafael.book;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperties(entry.getKey(), entry.getValue());
+
 		SpringApplication.run(BookApplication.class, args);
 	}
-
 }
