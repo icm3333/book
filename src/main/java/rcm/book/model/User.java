b7c453a47297f3a43d5510aa9fa9e3d29b82ea67
icm <trashhyuki@gmail.com>
2026-09-03 04:37:27 -0300

    fix: added jsonignore for user info

--- src/main/java/rcm/book/model/User.java
+++ src/main/java/rcm/book/model/User.java
@@ -1,5 +1,6 @@
 package rcm.book.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,6 +22,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 }
