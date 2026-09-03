85c9e651a62a585cdcc086fc76f9f81cbd3bc70c
icm <trashhyuki@gmail.com>
2026-09-03 04:04:46 -0300

    chore: created base bookController

--- src/main/java/rcm/book/controller/BookController.java
+++ src/main/java/rcm/book/controller/BookController.java
@@ -1,4 +1,56 @@
 package rcm.book.controller;
 
-public class BookController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rcm.book.dto.OpenLibraryResponseDTO;
+import rcm.book.model.ReadingStatus;
+import rcm.book.model.UserBook;
+import rcm.book.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController{
+
+    private final BookService bookService;
+
+
+    @GetMapping("/search")
+    public ResponseEntity<OpenLibraryResponseDTO> searchBooks(@RequestParam String query){
+        OpenLibraryResponseDTO result = bookService.searchBooksAPI(query);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/track")
+    public ResponseEntity<UserBook> addBookToUserList(
+                            @RequestParam Long userId,
+                            @RequestParam String openLibraryId,
+                            @RequestParam String title,
+                            @RequestParam(required = false) String author,
+                            @RequestParam(required = false) Integer pageCount){
+
+        UserBook trackedBook = bookService.addBookToUserList(userId, openLibraryId, title, author, pageCount);
+        return ResponseEntity.ok(trackedBook);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<UserBook>> getUserBooks(
+                            @PathVariable Long userId,
+                            @RequestParam(required = false)ReadingStatus status){
+
+        List<UserBook> books = bookService.getUserBooks(userId, status);
+        return ResponseEntity.ok(books);
+    }
+
+    @PatchMapping("/tracking/{userBookId}/progress")
+    public ResponseEntity<UserBook> updateProgress(
+                            @PathVariable Long userBookId,
+                            @RequestParam Integer page){
+
+        UserBook updated = bookService.updatePageProgress(userBookId, page);
+        return ResponseEntity.ok(updated);
+    }
 }
