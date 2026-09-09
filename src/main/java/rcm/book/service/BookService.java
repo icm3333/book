281cbb7a617e88672bafa0e98765df921401cb9a
icm <trashhyuki@gmail.com>
2026-09-08 22:47:32 -0300

    fix: changed to spring transactional

--- src/main/java/rcm/book/service/BookService.java
+++ src/main/java/rcm/book/service/BookService.java
@@ -1,6 +1,6 @@
 package rcm.book.service;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rcm.book.client.OpenLibraryClient;
